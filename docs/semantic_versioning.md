d5f3f564ad981653701cd7f3fed027e85a27316a
damatomos <damato578@gmail.com>
2023-02-07 14:26:30 -0300

    feat: add swagger, logger and docs

---
+++ docs/semantic_versioning.md
@@ -0,0 +1,18 @@
+## Versionamento
+
+### Como versionar a API
+
+Considerando o sistema de versões v1.0.0, utilizado de maneira
+semântica para futuras versões do projeto, temos:
+
+```
+[1].0.0 - no qual 1 se refere ao controle de compatibilidade, informa que existem
+funcionalidades/códigos incompatíveis com as versões anteriores. Dessa forma, se refere
+à uma nova release do projeto.
+
+0.[1].0 - no qual 1 se refere ao controle de funcionalidade, informa que novas funcionalidades
+foram adicionadas ao código. Dessa forma, se refere as features do projeto.
+
+0.0.[1] - no qual 1 se refere ao controle de correção de bugs, informa que um ou mais erros foram
+identificados e corrigidos. Dessa forma, se refere aos fix's do projeto.
+```
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+  });
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('A product api to front-end applications.')
+    .setVersion('0.1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
